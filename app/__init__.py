63e750f10ae5b4d9c95483de75b395c10a9cb30b
StevenBolc <steven.bolcar95@gmail.com>
2023-01-24 16:19:22 -0500

    setup app features

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from .routes import home
+from .routes import home, dashboard
 
 from flask import Flask
 def create_app(test_config=None):
@@ -16,5 +17,6 @@ def create_app(test_config=None):
 
 # register routes
   app.register_blueprint(home)
+  app.register_blueprint(dashboard)
 
   return app
